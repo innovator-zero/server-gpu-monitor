4af23c2f237e867dc9d6ed56f9e2d42abc4ccca2
innovator-zero <2529300178@qq.com>
2023-03-24 00:01:47 +0800

    2023.03.24 Version 1.01

--- ping.py
+++ ping.py
@@ -17,41 +17,33 @@ gpu_nums = pynvml.nvmlDeviceGetCount()
 handle_list = [pynvml.nvmlDeviceGetHandleByIndex(i) for i in range(gpu_nums)]
 
 
-def get_host_ip():
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.connect(('8.8.8.8', 80))
-        ip = s.getsockname()[0]
-        return ip
-
-
 def get_gpu_info():
     gpu_info = {}
     for i in range(len(handle_list)):
         meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle_list[i])
         gpu_info[i] = {
-            'name': pynvml.nvmlDeviceGetName(handle_list[i]),
-            'gpu_util': pynvml.nvmlDeviceGetUtilizationRates(handle_list[i]).gpu,
+            'name': str(pynvml.nvmlDeviceGetName(handle_list[i])),
+            'gpu_util': int(pynvml.nvmlDeviceGetUtilizationRates(handle_list[i]).gpu),
             'vram_status': '{:d}M/{:d}M'.format(int(meminfo.used / 2**20), int(meminfo.total / 2**20)),
-            'vram_util': round(meminfo.used / meminfo.total * 100)
+            'vram_util': int(meminfo.used / meminfo.total * 100)
         }
     return gpu_info
 
 
 if __name__ == "__main__":
-    body = {'host': host, 'gpu_nums': gpu_nums, 'gpu_info': {}, '_date': None}
+    body = {'host': host, 'gpu_nums': gpu_nums, 'gpu_info': {}, 'time_stamp': None}
     error_count = 0
     while True:
-        # body['ip'] = get_host_ip()
         body['gpu_info'] = get_gpu_info()
-        body['time_stamp'] = time.strftime('%Y-%m-%d %H:%M:%S')
+        body['time_stamp'] = time.strftime("%Y-%m-%d %H:%M:%S")
         success = False
         try:
             res = requests.post(target, json.dumps(body))
             if res.status_code == 200:
                 success = True
                 print('Success ping {:s}'.format(body['time_stamp']))
-        except Exception:
-            pass
+        except Exception as e:
+            print(e)
 
         if not success:
             error_count += 1
